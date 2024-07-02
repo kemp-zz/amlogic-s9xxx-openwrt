46d80ee5b51e4601a6879e69a1fafdb192b099d0
kemp-zz <fskemp@gmail.com>
2024-07-02 08:30:55 +0800

    Update diy-part1.sh

--- config/immortalwrt-master/diy-part1.sh
+++ config/immortalwrt-master/diy-part1.sh
@@ -13,10 +13,5 @@
 # 创建 package/emortal 目录
 mkdir -p package/emortal
 
-# 删除 autosamba 和 ipv6-helper 软件包（如果需要）
-rm -rf package/emortal/{autosamba,ipv6-helper}
 
-# ... (其他代码) ...
-# other
-# rm -rf package/emortal/{autosamba,ipv6-helper}
 
