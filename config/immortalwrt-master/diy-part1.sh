34a9aeac6404243b6def59eea51407a4cb652248
kemp-zz <fskemp@gmail.com>
2024-07-02 08:26:57 +0800

    Update diy-part1.sh

--- config/immortalwrt-master/diy-part1.sh
+++ config/immortalwrt-master/diy-part1.sh
@@ -9,6 +9,14 @@
 # Add a feed source
 # sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 
+
+# 创建 package/emortal 目录
+mkdir -p package/emortal
+
+# 删除 autosamba 和 ipv6-helper 软件包（如果需要）
+rm -rf package/emortal/{autosamba,ipv6-helper}
+
+# ... (其他代码) ...
 # other
 # rm -rf package/emortal/{autosamba,ipv6-helper}
 
