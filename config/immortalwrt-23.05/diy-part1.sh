3c26332bf4413bceb1e9b5955a5c56477a592354
kemp-zz <fskemp@gmail.com>
2024-07-21 16:53:25 +0800

    Update diy-part1.sh

--- config/immortalwrt-23.05/diy-part1.sh
+++ config/immortalwrt-23.05/diy-part1.sh
@@ -8,7 +8,7 @@
 
 # Add a feed source
 # sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-git clone https://github.com/immortalwrt/packages/tree/openwrt-23.05.git package/immortalwrt-packages
+git clone --branch openwrt-23.05 https://github.com/immortalwrt/packages.git package/immortalwrt-packages
 
 
 # other
