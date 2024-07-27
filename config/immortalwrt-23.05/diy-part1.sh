598c6f61b1fabb1514c3cc8f7026c504fca0bcb8
kemp-zz <fskemp@gmail.com>
2024-07-27 12:50:16 +0800

    Update diy-part1.sh

--- config/immortalwrt-23.05/diy-part1.sh
+++ config/immortalwrt-23.05/diy-part1.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
-#========================================================================================================================
-# https://github.com/ophub/amlogic-s9xxx-openwrt
-# Description: Automatically Build OpenWrt
-# Function: Diy script (Before Update feeds, Modify the default IP, hostname, theme, add/remove software packages, etc.)
-# Source code repository: https://github.com/immortalwrt/immortalwrt / Branch: master
-#========================================================================================================================
+# diy-part1.sh script for Amlogic S9xxx OpenWrt
 
-# Add a feed source
-# sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-
-
-
+# Clone the packages repository
 git clone -b openwrt-23.05 https://github.com/immortalwrt/packages.git
 
+# Set kernel version to 6.1.100
+KERNEL_VERSION="6.1.100"
 
+# Update kernel version in configuration
+sed -i "s/CONFIG_KERNEL_VERSION=\".*\"/CONFIG_KERNEL_VERSION=\"$KERNEL_VERSION\"/g" .config
 
+# Update kernel version in other relevant files if needed
+# For example, in target configuration files or Makefiles
+# Make sure to update this part according to your project's structure
 
-# other
-# rm -rf package/emortal/{autosamba,ipv6-helper}
+# Example: update kernel version in target Makefile
+# sed -i "s/^KERNEL_PATCHVER:=.*$/KERNEL_PATCHVER:=$KERNEL_VERSION/" target/linux/your_target/Makefile
 
+# Ensure configuration is updated
+make defconfig
+
+echo "Kernel version set to $KERNEL_VERSION"
